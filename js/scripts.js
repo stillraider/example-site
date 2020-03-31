91dfb29f2cb0b96378735bb8340492624402a16d
stillraider <stilryder74@mail.ru>
2020-03-31 22:22:59 +0500

    60%

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $(function(){
+    var checkboxs = $(".checkbox");
+
+    checkboxs.click(function(){
+        var mark = $(this).find(".check-mark");
+        if(mark.is(":hidden")) mark.show();
+        else mark.hide();
+    });
+
     $("#phone").mask("+7 999 999 9999");
-});
+})
